1fba55475e425eebad2177b83c4497a1c551cb2c
kc446 <kc446@njit.edu>
2022-05-11 19:16:56 -0400

    adding some files (a.k.a. shamelessly duplicating them from the other project for the sake of crunch time. i have a senior project to wrap up by tomorrow as well so bear with me okay)

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Production Workflow](https://github.com/kc446/kc446_flaskapp/actions/workflows/prod.yml/badge.svg)](https://github.com/kc446/flask-final/actions/workflows/prod.yml)
+
+* [Production Deployment](https://kc-flask-final.herokuapp.com/)
+
+[![Development Workflow](https://github.com/kc446/kc446_flaskapp/actions/workflows/dev.yml/badge.svg)](https://github.com/kc446/flask-final/actions/workflows/dev.yml)
+
+* [Developmental Deployment](https://kc-flask-final-dev.herokuapp.com/)
+
+Purpose: I've been struggling with senioritis so as long as i have something working that's the important thing.
--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,72 @@
-"""A simple flask web app"""
-import os
+"""A simple Flask web app."""
+
 from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_cors import CORS
+from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
+
+import flask_login
+from app.auth import auth
 from app.cli import create_database
+from app.context_processors import utility_text_processors
+from app.db import database
 from app.db import db
 from app.db.models import User
+from app.error_handlers import error_handlers
+from app.logging_config import log_con
+from app.simple_pages import simple_pages
 
 
-def page_not_found(e):
-    return render_template("404.html"), 404
+mail = Mail()
+login_manager = flask_login.LoginManager()
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    #trying something here to see if it's the port for some reason
+    #if __name__ == "__main__":
+        #port = int(os.environ.get("PORT", 5000))
+        #app.run(host='0.0.0.0', port=port)
 
-    app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+    if app.config["ENV"] == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif app.config["ENV"] == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif app.config["ENV"] == "testing":
+        app.config.from_object("app.config.TestingConfig")
+    app.mail = Mail(app)
+
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    csrf = CSRFProtect(app)
+    csrf.exempt(auth)
+    bootstrap = Bootstrap5(app)
+
+    # apply the blueprints to the app
+    app.register_blueprint(auth)
+    app.register_blueprint(database)
+    app.register_blueprint(simple_pages)
+
+    # these load functionality without a web interface
+    app.register_blueprint(log_con)
+    app.register_blueprint(error_handlers)
+
+    app.context_processor(utility_text_processors)
+
     # add command function to cli commands
     app.cli.add_command(create_database)
+    db.init_app(app)
 
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    api_v1_cors_config = {"methods": ["OPTIONS", "GET", "POST"]}
+    CORS(app, resources={"/api/*": api_v1_cors_config})
 
+    # Run once at startup
     return app
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
